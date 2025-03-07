180c72f8b3c4fee876d95c1049b7f51d67969b6c
Henrique Calaboni Godoy <henrique.c.godoy@hotmail.com>
2025-03-07 16:21:55 -0300

    Exercicio 2
    Aula 2 e 3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic; //List
 
 /// <summary>
 /// Manages the main gameplay of the game
@@ -12,12 +13,18 @@ public class GameManager : MonoBehaviour
     [Tooltip("A reference to the tile we want to spawn")]
     public Transform tile;
 
+    [Tooltip("A reference to the obstacle we want to spawn")]
+    public Transform obstacle;
+
     [Tooltip("Where the first tile should be placed at")]
     public Vector3 startPoint = new Vector3(0, 0, -5);
 
     [Tooltip("How many tiles should we create in advance")]
     [Range(1, 15)]
     public int initSpawnNum = 10;
+
+    [Tooltip("How many tiles to spawn with no obstacles")]
+    public int initNoObstacles = 4;
 
     /// <summary>
     /// Where the next tile should be spawned at.
@@ -37,7 +44,7 @@ public class GameManager : MonoBehaviour
 
         for (int i = 0; i < initSpawnNum; ++i)
         {
-            SpawnNextTile();
+            SpawnNextTile(i >= initNoObstacles);
         }
     }
 
@@ -45,7 +52,8 @@ public class GameManager : MonoBehaviour
     /// Will spawn a tile at a certain location and
     /// setup the next position
     /// </summary>
-    public void SpawnNextTile()
+    /// <param name="spawnObstacles">If we should spawn an obstacle</param>
+    public void SpawnNextTile(bool spawnObstacles = true)
     {
         var newTile = Instantiate(tile, nextTileLocation, nextTileRotation);
         
@@ -54,6 +62,45 @@ public class GameManager : MonoBehaviour
         nextTileLocation = nextTile.position;
         nextTileRotation = nextTile.rotation;
 
+        if (spawnObstacles)
+        {
+            SpawnObstacle(newTile);
+        }
+
+    }
+
+    private void SpawnObstacle(Transform newTile)
+    {
+        //Now we need to get all of the possible places to spawn te obstacle
+        var obstacleSpawnPoints = new List<GameObject>();
+
+        //Go through each of the children in our tile
+        foreach (Transform child in newTile)
+        {
+            //if it has ObstacleSpawn tag
+            if (child.CompareTag("ObstacleSpawn"))
+            {
+                //We add it as a possibility
+                obstacleSpawnPoints.Add(child.gameObject);
+            }
+        }
+
+        //Make sure there is at least one
+        if (obstacleSpawnPoints.Count > 0)
+        {
+            //Get random spawn point from the ones we have
+            int index = UnityEngine.Random.Range(0, obstacleSpawnPoints.Count);
+            var spawnPoint = obstacleSpawnPoints[index];
+
+            //Store its position for us to use
+            var spawnPos = spawnPoint.transform.position;
+
+            //Create our obstacle
+            var newObstacle = Instantiate(obstacle, spawnPos, Quaternion.identity);
+
+            //Have it parented to the tile
+            newObstacle.SetParent(spawnPoint.transform);
+        }
     }
 
 }
